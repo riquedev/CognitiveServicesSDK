7f22ab053e3e53db091eeeb333c6d65424a39f1f
Henrique da Silva Santos <rique_dev@hotmail.com>
2017-08-05 10:20:42 -0300

    Atualização na Doc

--- core/GenerateThumbnailHelper.php
+++ core/GenerateThumbnailHelper.php
@@ -2,35 +2,73 @@
 
 namespace rqdev\packages\ComputerVisionAPI\GenerateThumbnail;
 
+/**
+ *  Esta classe tem o objetivo de auxiliar na manipulação do retorno
+ *  binário da API.
+ * 
+ *  @author Henrique da Silva Santos < rique_dev@hotmail.com >
+ *  @copyright (c) 2017, Henrique da Silva Santos
+ *  @license https://opensource.org/licenses/MIT
+ *  @version 1.0.5
+ * 
+ */
 trait ImageProperties {
 
+    /**
+     * @var string|base64 Dados já convertidos. 
+     */
     static protected $data;
+
+    /**
+     * @var array Propriedades da imagem.
+     */
     static protected $properties;
 
+    /**
+     * @return string Nome do arquivo gerado.
+     */
     static public function FileName() {
         return self::$properties['FileName'];
     }
 
+    /**
+     * @return mixed
+     */
     static public function FileDateTime() {
         return self::$properties['FileDateTime'];
     }
 
+    /**
+     * @return integer Tamanho do arquivo.
+     */
     static public function FileSize() {
         return self::$properties['FileSize'];
     }
 
+    /**
+     * @return integer Tipo do arquivo.
+     */
     static public function FileType() {
         return self::$properties['FileType'];
     }
 
+    /**
+     * @return string MimeType do arquivo
+     */
     static public function MimeType() {
         return self::$properties['MimeType'];
     }
 
+    /**
+     * @return string Seções encontradas no arquivo.
+     */
     static public function SectionsFound() {
         return self::$properties['SectionsFound'];
     }
 
+    /**
+     * @return array Propriedades gerais do arquivo.
+     */
     static public function getProperties() {
         return self::$properties;
     }
@@ -39,16 +77,25 @@ trait ImageProperties {
 
 class Helper {
 
+    /**
+     * @var string|binary Dados binários do arquivo. 
+     */
     static protected $imageData;
 
     public function __construct(string $imageData) {
         self::$imageData = $imageData;
     }
 
+    /**
+     * @return string|base64 Converte o arquivo para base64.
+     */
     static public function toBase64() {
         return base64_encode(self::$imageData);
     }
 
+    /**
+     * @return string Obtem HTML para visualizar imagem.
+     */
     static public function getHtml() {
         $details = self::ImageDetails();
         $data = 'data://' . $details::MimeType() . ';base64,' . self::toBase64();
@@ -56,6 +103,9 @@ class Helper {
         return $str;
     }
 
+    /**
+     * @return class|anonymous Classe anônima para obter dados da imagem.
+     */
     static public function ImageDetails() {
 
         return new class(self::toBase64()) {
