3d3512bfcb4285bd69c67bf19d707c23ffcfc3ab
Henrique da Silva Santos <rique_dev@hotmail.com>
2017-08-05 14:30:55 -0300

    Novos Exemplos

---
+++ examples/ListDomainSpecificModels/index.php
@@ -0,0 +1,48 @@
+<?php
+
+header("Content-type:application/json");
+// ../../..
+$MainPath = realpath(dirname(dirname(dirname(__FILE__))));
+
+// Incluindo arquivo correspondente a classe.
+require_once($MainPath . '\core\settings.php');
+require_once($MainPath . '\core\ListDomainSpecificModels.php');
+
+// Localizações da requisição
+$RequestLocation = CVA_API_LOCATION_ARRAY;
+
+
+// Usar header principal?
+$useMainHeader = True;
+
+// Instanciando classe...
+$Analyze = new \rqdev\packages\ComputerVisionAPI\ListDomainSpecificModels();
+
+$Sucess = $Analyze->setAPILocation(
+                $RequestLocation[2]
+        )->Send($useMainHeader);
+
+if ($Sucess) {
+    // Resposta
+    $ResponseObject = $Analyze->response;
+
+    // Pretty-Print
+    $prettyJson = true;
+
+    // Json
+    $JsonResponse = $ResponseObject->toJson($prettyJson);
+
+    // Array
+    $ArrayResponse = $ResponseObject->toArray();
+
+    // Objeto
+    $ObjectResponse = $ResponseObject->toObject();
+
+    // Resposta JSON
+    echo $JsonResponse;
+} else {
+    var_dump($Analyze->error);
+}
+
+
+
